8b420ff21231a09df84a705a3eb0dcc727407f5e
antonmry <anton@galiglobal.com>
2017-04-13 18:56:46 +0200

    Input validation

--- 201704/antonmry/BasicFormatter.groovy
+++ 201704/antonmry/BasicFormatter.groovy
@@ -29,9 +29,23 @@ class BasicFormatter {
         input
     }
 
+    void finishWithError(String error) {
+        println "You have to insert at least 1 line"
+        System.exit(-1)
+    }
+
     void run() {
+        // Retrieve the number of lines
         def input = getInput("", 0)
-        println input
+        int numberLines = input.isInteger() ? input.toInteger() : null
+        if (numberLines < 1) finishWithError("You have to insert a number bigger than 0")
+
+        // Retrieve indent char
+        def indStr = getInput("", "")
+
+        (1..numberLines).forEach() {
+            input = getInput("", "")
+        }
     }
 }
 
