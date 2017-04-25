01d468d4b429f323b77f19a815a3dd280f644814
Carlos Goce <carlosgoce@gmail.com>
2017-04-26 01:06:19 +0200

    Fix test

--- carlosgoce/autoindent.rb
+++ carlosgoce/autoindent.rb
@@ -1,16 +1,20 @@
-current_indent = 0
-open_op = ['FOR', 'IF']
-close_op = ['ENDIF', 'NEXT']
+module DinoSetoSpace
+  def self.autoindent(input)
+    current_indent = 0
+    open_op = ['FOR', 'IF']
+    close_op = ['ENDIF', 'NEXT']
 
-input = File.readlines('input')
-indent = input[1].length
-indent_with = input[1][0]
+    indent = input[1].length - 1
+    indent_with = input[1][0]
 
-lines = input.drop(2).each_with_index.map {|line, i|
-  line = line.delete('·»')
-  current_indent += indent if line.start_with?('FOR', 'IF')
-  current_indent -= indent if line.start_with?('ENDIF', 'NEXT')
-  indent_with * current_indent + line
-}
+    lines = input.drop(2).each_with_index.map {|line, i|
+      line = line.delete('·»')
+      current_indent -= indent if line.start_with?('ENDIF', 'NEXT')
+      new_line = indent_with * current_indent + line
+      current_indent += indent if line.start_with?('FOR', 'IF')
+      new_line
+    }
 
-puts lines
+    lines.join
+  end
+end
---
+++ carlosgoce/test_autoindent.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require "./autoindent.rb"
+
+class TestAutoindent < Minitest::Test
+  def test_basic_input
+    expected = File.read('output')
+    assert_equal expected, DinoSetoSpace::autoindent(File.readlines('input'))
+  end
+end
