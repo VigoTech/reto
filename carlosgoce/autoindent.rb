0df5ae165e4db040448e0f18d950445981ba32a8
Carlos Goce <carlosgoce@gmail.com>
2017-04-26 07:57:53 +0200

    Refactor

--- carlosgoce/autoindent.rb
+++ carlosgoce/autoindent.rb
@@ -1,14 +1,13 @@
 module DinoSetoSpace
   def self.autoindent(input)
-    current_indent = 0
-    indent = input[1].length - 1
-    indent_with = input[1][0]
+    lines = input.lines
+    current_indent, indent_width, char = [0, lines[1].length-1, lines[1][0]]
 
-    input.drop(2).map {|line|
+    lines.drop(2).map {|line|
       line = line.delete('·»')
-      current_indent -= indent if line.start_with?('ENDIF', 'NEXT')
-      new_line = indent_with * current_indent + line
-      current_indent += indent if line.start_with?('FOR', 'IF')
+      current_indent -= indent_width if line.start_with?('ENDIF', 'NEXT')
+      new_line = char * current_indent + line
+      current_indent += indent_width if line.start_with?('FOR', 'IF')
       new_line
     }.join
   end
--- carlosgoce/test_autoindent.rb
+++ carlosgoce/test_autoindent.rb
@@ -4,6 +4,6 @@ require "./autoindent.rb"
 class TestAutoindent < Minitest::Test
   def test_basic_input
     expected = File.read('output')
-    assert_equal expected, DinoSetoSpace::autoindent(File.readlines('input'))
+    assert_equal expected, DinoSetoSpace::autoindent(File.read('input'))
   end
 end
