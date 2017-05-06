2e3c918b50eac2d782252683dd04e40b6efacfd6
Alex Hermida <alexhermida@gmail.com>
2017-05-06 21:21:44 +0200

    Test check adding elements

--- 201705/alexhermida/reto.py
+++ 201705/alexhermida/reto.py
@@ -1,3 +1,4 @@
+from itertools import accumulate
 
 
 def check_opposite_number_in(integers_list):
@@ -7,3 +8,14 @@ def check_opposite_number_in(integers_list):
         if item == 0 or item in integers_list:
             return True
     return False
+
+
+def check_adding_elements(integers_list):
+    list_copy = integers_list[:]
+
+    if 0 in list(accumulate(list_copy)):
+        return True
+
+    for item in list_copy:
+        if check_opposite_number_in(item, integers_list):
+            return True
--- 201705/alexhermida/tests/test_reto.py
+++ 201705/alexhermida/tests/test_reto.py
@@ -1,9 +1,9 @@
 import pytest
 
-from reto import check_opposite_number_in
+from reto import check_opposite_number_in, check_adding_elements
 
 
-@pytest.mark.parametrize('list, result', [
+@pytest.mark.parametrize('int_list, result', [
     ([1, 2, 3], False),
     ([-5, -3, -1, 2, 4, 6], False),
     ([], False),
@@ -12,5 +12,14 @@ from reto import check_opposite_number_in
     ([-53974, -39140, -36561, -23935, -15680, 0], True),
     ([-2, 2, 3], True),
 ])
-def test_examples(list, result):
-    assert check_opposite_number_in(list) == result
+def test_examples(int_list, result):
+    assert check_opposite_number_in(int_list) == result
+
+
+@pytest.mark.parametrize('int_list, result', [
+    ([0], True),
+    ([-3, 1, 2], True),
+    ([-98634, -86888, -48841, -40483, 2612, 9225, 17848, 71967, 84319, 88875], True),
+])
+def test_optional_bonus(int_list, result):
+    assert check_adding_elements(int_list) == result
