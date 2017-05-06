804b83a435d008422ce46da1ca6fdc746d5bce64
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-05-06 22:49:28 +0200

    no need of impossible check

--- 201705/daavoo/solve.py
+++ 201705/daavoo/solve.py
@@ -1,40 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Reto 201705.
+- Reto 201705.
 
 Dada unha lista ordenada de enteiros, escribir unha función que devolva si hai
 dous enteiros na lista que sumen 0.
 Por exemplo, devolvería true si na lista hai os valores -14435 e 14335,
 xa qué -14435 + 14335 = 0.
 Tamén devolverá true si o 0 aparece na lista.
+
+- Bonus opcional
+
+O reto de este mes é unha versión simplificada do problema de sumar
+subconxuntos. O bonus consiste en resolver o problema completo.
+Dada unha lista de distintos enteiros, escribir unha función que devolva si hai
+algun subconxunto non-vacío de enteiros que sumen 0.
 """
 import collections
-
-
-def impossible_no_bonus(ordered_list, num):
-    """
-    Fast check of impossible cases for no-bonus challenge.
-
-    - Minimum num in ordered_list < num.
-    - ordered_list must be ordered
-    - ordered_list is empty
-
-    Parameters
-    ----------
-    ordered_list: list of int
-    num: int
-
-    Return
-    ------
-    bool
-    """
-    try:
-        if ordered_list[0] > num:
-            return True
-        elif ordered_list[0] > ordered_list[-1]:
-            return True
-    except IndexError:
-        return True
 
 
 # Solver 1
@@ -77,29 +58,6 @@ def deque_solver(ordered_list, num):
         return False
 
 
-def exist_subset(ordered_list, num, impossible, solver):
-    """
-    Wrapper for different impossible cases and solvers.
-
-    Parameters
-    ----------
-    ordered_list: list of int
-    num: int
-    impossible: function(ordered_list, num)
-        Fast check of impossible cases
-    solver: function(ordered_list, num)
-        Should solve all possible cases.
-
-    Return
-    ------
-    bool
-    """
-    if impossible(ordered_list, num):
-        return False
-    else:
-        return solver(ordered_list, num)
-
-
 if __name__ == '__main__':
     import pickle
     with open("test_cases_no_bonus.pk", "rb") as f:
@@ -111,10 +69,7 @@ if __name__ == '__main__':
         true_output = case[1]
         print("\nCASE: {}".format(ordered_list))
         print("EXPECTED VALUE: {}".format(true_output))
-        our_output = exist_subset(ordered_list,
-                                  N,
-                                  impossible_no_bonus,
-                                  deque_solver)
+        our_output = deque_solver(ordered_list, N)
         assert our_output == true_output
 
     print("\nAll test cases passed\n")
