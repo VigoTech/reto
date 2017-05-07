5722c9c3adf4f2e9b995e4cb5a01e228f673c598
Daniel Rodríguez Gil <daniel@dpstudios.es>
2017-05-07 13:21:18 +0200

    Implement concurrency in a better way

--- 201705/danybmx/README.md
+++ 201705/danybmx/README.md
@@ -5,8 +5,11 @@
 O meu obxectivo foi volver un pouco a Go e traballar con channels minimamente xa
 que é algo que me custa entender.
 
-Finalmente descartéi os channels xa que facían que fose máis lento, seguro que
-o estaba a facer mal.
+Había descartado a concurrencia xa que facía que fose máis lento o proceso, pero resulta
+que me acabei dando conta de que o pasar información usando channels consume moitos
+recursos entón non era boa idea procesar cada operación nunha goroutine. Finalmente
+dividín o número de operacións a realizar entre o numero de procesos que se van a lanzar
+e calcular así en cada proceso procesos/workers.
 
 Para executalo, soamente necesitas por os ficheiros no teu GOPATH e facer un
 `go test` e se queres ver un pouca máis de información `go test -v -test.timeout 60m`.
--- 201705/danybmx/sumacero.go
+++ 201705/danybmx/sumacero.go
@@ -2,30 +2,53 @@ package sumacero
 
 import (
 	"math"
+	"runtime"
 	"strconv"
+	"sync"
 	"time"
 )
 
-func hasZeroSet(numbers []int) (result bool, elapsed float64) {
+func calculate(result *bool, wg *sync.WaitGroup, nOptions int, nTotalOptions int, offset int, nNumbers int, numbers []int) {
+	for x := 0; x < nOptions; x++ {
+		if *result || nTotalOptions <= x+offset {
+			break
+		}
+		sum := 0
+		option := strconv.FormatInt(int64(x+offset+1), 2)
+		offset := nNumbers - len(option)
+		for z := 0; z < nNumbers; z++ {
+			if offset-1 < z && option[z-offset] == 49 {
+				sum += numbers[z]
+			}
+		}
+		if sum == 0 {
+			*result = true
+		}
+	}
+	wg.Done()
+}
+
+func hasZeroSet(numbers []int) (bool, float64) {
 	start := time.Now()
 	nNumbers := len(numbers)
 	nOptions := int(math.Pow(2, float64(nNumbers))) - 1
-	result = false
-	if nOptions > 0 && !result {
-		for x := 0; x < nOptions; x++ {
-			sum := 0
-			option := strconv.FormatInt(int64(x+1), 2)
-			offset := nNumbers - len(option)
-			for z := 0; z < nNumbers; z++ {
-				if offset-1 < z && option[z-offset] == 49 {
-					sum += numbers[z]
-				}
-			}
-			if sum == 0 {
-				result = true
+	availableWorkers := runtime.GOMAXPROCS(-1)
+	result := false
+	var wg sync.WaitGroup
+
+	if nOptions > 0 {
+		if nOptions < availableWorkers {
+			wg.Add(1)
+			go calculate(&result, &wg, nOptions, nOptions, 0, nNumbers, numbers)
+		} else {
+			nOperationsPerWorker := int(math.Ceil(float64(nOptions) / float64(availableWorkers)))
+			for x := 0; x < availableWorkers; x++ {
+				wg.Add(1)
+				go calculate(&result, &wg, nOperationsPerWorker, nOptions, x*nOperationsPerWorker, nNumbers, numbers)
 			}
 		}
 	}
-	elapsed = time.Since(start).Seconds()
-	return
+	wg.Wait()
+
+	return result, time.Since(start).Seconds()
 }
