cc80c9badab8bdc3ee142e029b8cf55642552368
Alex Hermida <alexhermida@gmail.com>
2017-05-14 00:27:32 +0200

    Adding tests:

--- 201705/alexhermida/rust/src/main.rs
+++ 201705/alexhermida/rust/src/main.rs
@@ -1,3 +1,30 @@
+#[macro_use]
+extern crate itertools;
+
+use itertools::Itertools;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let list_1 = vec![-97364, -71561, -69336, 19675, 71561, 97863];
+    println!("List: {:?}", list_1);
+
+    for i in (1..list_1.len()).rev() {
+        println!("Element: {:?}", i);
+
+        let iter = list_1.iter();
+
+        for item in iter.combinations(i) {
+            println!("itemm {:?}", item);
+            let a = item.into_iter();
+            let sum: i32 = a.sum();
+            println!("itemm sum {:?}", sum);
+
+
+            if sum == 0 {
+                 println!("entraaaaaaaa");
+            }
+        }
+
+    }
 }
