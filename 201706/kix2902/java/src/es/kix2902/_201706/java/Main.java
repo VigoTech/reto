fe9d9f2aeb259b5221acaf43edeb12ff86262030
Ismael Reyes <ismael.kix2902@gmail.com>
2017-06-12 19:51:43 +0200

    Added tests

--- 201706/kix2902/java/src/es/kix2902/_201706/java/Main.java
+++ 201706/kix2902/java/src/es/kix2902/_201706/java/Main.java
@@ -9,12 +9,16 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         if (args.length != 1) {
             System.err.print("You must include input filename as parameter");
             System.exit(1);
         }
 
+        System.out.println(processFile(args));
+    }
+
+    public static String processFile(String... args) {
         try {
             Scanner input = new Scanner(new File(args[0]));
 
@@ -22,8 +26,6 @@ public class Main {
             do {
                 line = input.nextLine();
             } while (line.startsWith("#"));
-
-            int coins_total = Integer.valueOf(line);
 
             int[] coins_values = Arrays.stream(input.nextLine().trim().split("\\s+"))
                     .map(String::trim).mapToInt(Integer::parseInt).toArray();
@@ -61,14 +63,15 @@ public class Main {
                 }
             }
 
-            System.out.println(String.join(" ", coins_change.stream().map(Object::toString)
-                    .collect(Collectors.joining(" "))));
+            return String.join(" ", coins_change.stream().map(Object::toString).collect(Collectors.joining(" ")));
 
         } catch (FileNotFoundException e) {
-            System.err.println("File not found");
+            return "File not found";
 
         } catch (NumberFormatException e) {
-            System.err.println("Input it's not well formatted");
+            return "Input it's not well formatted";
         }
+
     }
+
 }
